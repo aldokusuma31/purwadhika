1c0b608740f996f6701b0999a2f3a737ef27b79b
aldokusuma31 <aldo.kusumawardana@gmail.com>
2024-05-02 20:57:08 +0700

    first init

---
+++ firstCode.js
@@ -0,0 +1,19 @@
+// Sesi 1 JCWDOL
+/* ini catatan
+2 baris atau lebih */
+
+console.log("Hello World!");
+
+let message; // membuat variable bernama message
+message = "Hello"; // mengisi nilai kedalam variable message
+
+var globalvariable = "Hello World!";
+
+let replaceable = "This value will be replaced"
+replaceable = "I Love Javascript"
+
+const constant = "Hello Purwadhika"
+
+console.log(globalvariable)
+console.log(replaceable)
+console.log(constant)
---
+++ loop.js
@@ -0,0 +1,27 @@
+// for (let i = 0 ; i<3 ; i++) {
+//     console.log("Hello! " + i)
+// }
+
+// let i = 0
+// while (i < 3) {
+//     console.log("Hello! " + i)
+//     i++
+// }
+
+// // while mikir dulu sebelum jalan
+// let i = 5
+
+// while(i < 5) {
+//     console.log("Hello World! ")
+// }
+
+// // do While, jalan dulu baru mikir
+// do {
+//     console.log("Hello World do While")
+//     i++
+// } while (i < 5)
+
+for(let i = 0; i < 5; i++){
+    if (i == 3) continue
+    console.log(i)
+}
